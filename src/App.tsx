172d14cbf7ac63b2a68d1f588f31ed369a67e038
Vivien Tran <vivitr9@gmail.com>
2025-04-27 17:47:13 +1000

    base page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // setting prompt
+  const prompts = [
+    "What are you grateful for?",
+    "What are you excited for?", 
+    "What are you curious about?", 
+    "What are you feeling?",
+    "What are you thinking about?",
+    "What are you doing right now?",
+    "What did you dream about?",
+    "What are you doing tomorrow?", 
+    "What are you looking forward to?", 
+  ];
+
+  const [prompt, setPrompt] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * prompts.length);
+    setPrompt(prompts[randomIndex]);
+  }, []);
+
+  // setting rich values
+  const richValues = [
+    "life",
+    "love", 
+    "kindness", 
+    "time", 
+    "wisdom", 
+    "gratitude", 
+  ];
+
+  const [richValue, setRichProp] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * richValues.length);
+    setRichProp(richValues[randomIndex]);
+  }, []);
+
+  // auto-resize text area
+  const autoResize = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    e.target.style.height = `${e.target.scrollHeight}px`; // Set height based on content
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="flex flex-col items-center w-full min-w-full px-10 pt-10"> 
+
+      <textarea id="entry" 
+        placeholder={prompt} 
+        onInput={autoResize}
+        className="w-full border-b-3 border-red-500 text-3xl sm:text-5xl" />
+      <button className="bg-red-500 hover:bg-pink-500 text-white w-full py-3 px-20 mt-10 rounded-xl text-3xl sm:text-5xl">I am rich in {richValue}</button>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
