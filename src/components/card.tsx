0f10c90ede3481e7603edb238e5184b7a718d648
Vivien Tran <vivitr9@gmail.com>
2025-04-29 22:28:57 +1000

    add components

---
+++ src/components/card.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { format } from "date-fns";
+
+interface CardProps {
+  mp3?: string;
+  artist?: string;
+  book?: string;
+  author?: string;
+  mp4?: string;
+  description: string;
+}
+
+const formatItems = (
+  mp3?: string,
+  artist?: string,
+  book?: string,
+  author?: string,
+  mp4?: string,
+) => {
+  const items = [];
+  if (mp3 && artist) {
+    items.push({
+      text: `🎧\u00A0\u00A0${(mp3 || "").toUpperCase()}_${(artist || "").toUpperCase()}`,
+    });
+  }
+  if (mp3 && !artist) {
+    items.push({ text: `🎧\u00A0\u00A0${(mp3 || "").toUpperCase()}` });
+  }
+  if (!mp3 && artist) {
+    items.push({ text: `🎧\u00A0\u00A0${(artist || "").toUpperCase()}` });
+  }
+  if (book && author) {
+    items.push({
+      text: `📖\u00A0\u00A0${(book || "").toUpperCase()}_${(author || "").toUpperCase()}`,
+    });
+  }
+  if (book && !author) {
+    items.push({ text: `📖\u00A0\u00A0${(book || "").toUpperCase()}` });
+  }
+  if (!book && author) {
+    items.push({ text: `📖\u00A0\u00A0${(author || "").toUpperCase()}` });
+  }
+  if (mp4) {
+    items.push({ text: `🎬\u00A0\u00A0${(mp4 || "").toUpperCase()}` });
+  }
+  return items;
+};
+
+const Card: React.FC<CardProps> = ({
+  mp3,
+  artist,
+  book,
+  author,
+  mp4,
+  description,
+}) => {
+  const now = new Date();
+  const formattedDate = format(now, "EEEE dd MMM yyyy h:mmaaa");
+  const items = formatItems(mp3, artist, book, author, mp4);
+
+  return (
+    <label className="block col-span-1">
+      <input type="checkbox" className="peer hidden absolute scale-0" />
+      <span className="block p-5 rounded-xl peer-checked:rounded-b-none bg-white hover:bg-fuchsia-50 transition-all duration-200 delay-100 hover:scale-101 ease-in-out peer-checked:hover:scale-100">
+        <p className="text-red-500 text-lg md:text-xl lg:text-2xl text-center mb-3">
+          {formattedDate}
+        </p>
+        {items.map((item, index) => (
+          <p
+            key={index}
+            className="font-mono text-pink-500 md:font-semibold text-xl text-xs text-left px-4 mb-1"
+          >
+            {item.text}
+          </p>
+        ))}
+      </span>
+      <span className="block overflow-hidden rounded-b-xl bg-white max-h-0 peer-checked:max-h-screen transition-all duration-170 peer-checked:delay-300">
+        <p
+          className="p-5 text-slate-700 text-sm w-full"
+          style={{
+            whiteSpace: "pre-wrap",
+            wordWrap: "break-word",
+            hyphens: "auto",
+          }}
+        >
+          {description}
+        </p>
+      </span>
+    </label>
+  );
+};
+
+export default Card;
---
+++ src/components/currents.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+interface CurrentsProps {
+  showCurrents: boolean;
+  setShowCurrents: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Currents: React.FC<CurrentsProps> = ({
+  showCurrents,
+  setShowCurrents,
+}) => {
+  return (
+    showCurrents && (
+      <div className="absolute top-13 end-10 rounded-lg border-2 border-red-500 bg-white/90 px-8 py-4 w-70">
+        <label className="block font-mono font-semibold text-red-500 text-sm mb-5">
+          🎧 SONG/ALBUM<br></br>
+          <input
+            type="text"
+            name="mp3"
+            placeholder="CURRENT MUSIC ON REPEAT?"
+            autoComplete="off"
+            className="w-full border-b border-red-500 text-slate-600 font-sans text-sm placeholder:font-mono placeholder:text-xs"
+          ></input>
+        </label>
+        <label className="block font-mono font-semibold text-red-500 text-sm mb-5">
+          🎧 ARTIST<br></br>
+          <input
+            type="text"
+            name="artist"
+            placeholder="WHO CREATED IT?"
+            autoComplete="off"
+            className="w-full border-b border-red-500 text-slate-600 font-sans text-sm placeholder:font-mono placeholder:text-xs"
+          ></input>
+        </label>
+        <label className="block font-mono font-semibold text-red-500 text-sm mb-5">
+          📖 BOOK<br></br>
+          <input
+            type="text"
+            name="book"
+            placeholder="CURRENTLY READING?"
+            autoComplete="off"
+            className="w-full border-b border-red-500 text-slate-600 font-sans text-sm placeholder:font-mono placeholder:text-xs"
+          ></input>
+        </label>
+        <label className="block font-mono font-semibold text-red-500 text-sm mb-5">
+          📖 AUTHOR<br></br>
+          <input
+            type="text"
+            name="author"
+            placeholder="WHO WROTE IT?"
+            autoComplete="off"
+            className="w-full border-b border-red-500 text-slate-600 font-sans text-sm placeholder:font-mono placeholder:text-xs"
+          ></input>
+        </label>
+        <label className="block font-mono font-semibold text-red-500 text-sm mb-5">
+          🎬 MOVIE/TV SHOW<br></br>
+          <input
+            type="text"
+            name="mp4"
+            placeholder="LAST WATCHED/WATCHING?"
+            autoComplete="off"
+            className="w-full border-b border-red-500 text-slate-600 font-sans text-sm placeholder:font-mono placeholder:text-xs"
+          ></input>
+        </label>
+        <div className="flex flex-row justify-between items-center">
+          <button
+            className="w-20 rounded-full bg-red-500 hover:bg-red-600 hover:shadow-sm font-mono font-semibold text-white text-sm px-4 py-2 hover:scale-105 ease-in-out duration-200"
+            onClick={() => {
+              const inputs = document.querySelectorAll("input");
+              inputs.forEach((input) => (input.value = ""));
+            }}
+          >
+            RESET
+          </button>
+          <button
+            className="w-20 rounded-full bg-red-500 hover:bg-red-600 hover:shadow-sm font-mono font-semibold text-white text-sm px-4 py-2 hover:scale-105 ease-in-out duration-200"
+            onClick={() => {
+              setShowCurrents(false);
+            }}
+          >
+            SAVE
+          </button>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Currents;
